7ef132afb3d46dcb6d1bb74e9e22b5cd81fc804d
Madison Thantu <madisonthantu@gmail.com>
2023-11-06 15:33:19 -0500

    Merge branch 'main' of https://github.com/madisonthantu/recursive_LLMs

---
+++ utils.py
@@ -0,0 +1,8 @@
+def compute_coverage(document_toks, summary_toks):
+    """_summary_
+    Args:
+        document_toks (List[Int]): _description_
+        summary_toks (_type_): _description_
+    REF: HydraSum: Disentangling Style Features in Text Summarization with Multi-Decoder Models
+    """
+    return
